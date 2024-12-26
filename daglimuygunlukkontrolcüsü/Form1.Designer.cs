c913ac561c86905758cd45921c1e1823bb240f0b
efeketket <117019767+efeketket@users.noreply.github.com>
2024-12-27 01:09:55 +0300

    Add files via upload

---
+++ daglimuygunlukkontrolcüsü/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace daglimuygunlukkontrolcüsü
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(761, 678);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(761, 716);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Chi-Kare Testi :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(761, 753);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "p-testi : ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1221, 798);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 49);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 798);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 49);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Dosya Yükle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 72;
+            this.dataGridView1.RowTemplate.Height = 31;
+            this.dataGridView1.Size = new System.Drawing.Size(1333, 634);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(213, 678);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(105, 29);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 682);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(188, 25);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Anlamlıkık Düzeyi %";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(185, 798);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 49);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Kaydet";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1388, 859);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ daglimuygunlukkontrolcüsü/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using MathNet.Numerics.Distributions;
+
+
+namespace daglimuygunlukkontrolcüsü
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = 6;
+            dataGridView1.Columns[0].Name = "xᵢ";
+            dataGridView1.Columns[1].Name = "nᵢ";
+            dataGridView1.Columns[2].Name = "xᵢ * nᵢ";
+            dataGridView1.Columns[3].Name = "xᵢ² * nᵢ";
+            dataGridView1.Columns[4].Name = "xᵢ³ * nᵢ";
+            dataGridView1.Columns[5].Name = "xᵢ⁴ * nᵢ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // OpenFileDialog oluştur
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Dosya Seç",
+                Filter = "Metin Dosyaları (*.txt)|*.txt|Tüm Dosyalar (*.*)|*.*",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                // DataReader ile dosyayı yükle
+                var dataReader = new DataReader();
+                try
+                {
+                    dataReader.LoadData(filePath);
+
+                    // DataGridView'i doldur
+                    dataGridView1.Rows.Clear(); // Önce eski verileri temizleyelim
+                    for (int i = 0; i < dataReader.XiList.Count; i++)
+                    {
+                        int xi = dataReader.XiList[i];
+                        int ni = dataReader.NiList[i];
+                        int xi_ni = xi * ni;
+                        int xi2_ni = xi * xi * ni;
+                        int xi3_ni = xi * xi * xi * ni;
+                        int xi4_ni = xi * xi * xi * xi * ni;
+
+                        // Tabloya satır ekle
+                        dataGridView1.Rows.Add(xi, ni, xi_ni, xi2_ni, xi3_ni, xi4_ni);
+                    }
+
+                    MessageBox.Show("Veriler başarıyla yüklendi!", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Hata: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double alpha = Convert.ToDouble(textBox1.Text);
+            if (!double.TryParse(textBox1.Text, out alpha) || alpha <= 0 || alpha >= 1)
+            {
+                MessageBox.Show("Lütfen 0 ile 1 arasında geçerli bir anlamlılık düzeyi girin (ör: 0.05)", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Gözlenen frekanslar (n_i)
+            var observed = new List<int>();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[1].Value != null)
+                    observed.Add(int.Parse(row.Cells[1].Value.ToString()));
+            }
+
+            // Beklenen frekanslar
+            var expected = new List<double>();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells["BeklenenFrekans"].Value != null)
+                    expected.Add(double.Parse(row.Cells["BeklenenFrekans"].Value.ToString()));
+            }
+
+            // Serbestlik derecesi
+            int degreesOfFreedom = observed.Count - 1 - 1; // Kategori Sayısı - 1 - Tahmin Edilen Parametreler
+
+            // Chi-Kare Testi
+            var chiSquareTest = new ChiSquareTest();
+            double chiSquare = chiSquareTest.CalculateChiSquare(observed, expected);
+            double pValue = chiSquareTest.CalculatePValue(chiSquare, degreesOfFreedom);
+
+            // Anlamlılık düzeyine göre karar ver
+            
+            label2.Text = $"Chi-Kare Değeri: {chiSquare:F2}";
+            label3.Text = $"p - Değeri: {pValue} ";
+            if (pValue < alpha)
+            {
+                MessageBox.Show($"Chi-Kare Değeri: {chiSquare:F2}, p-Değeri: {pValue:F4}\nSonuç: Poisson dağılımına uygun değil.", "Test Sonucu");
+            }
+            else
+            {
+                MessageBox.Show($"Chi-Kare Değeri: {chiSquare:F2}, p-Değeri: {pValue:F4}\nSonuç: Poisson dağılımına uygun.", "Test Sonucu");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // DataGridView'den verileri oku
+            var xiList = new List<int>();
+            var niList = new List<int>();
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                // Eğer satır boş değilse
+                if (row.Cells[0].Value != null && row.Cells[1].Value != null)
+                {
+                    int xi = int.Parse(row.Cells[0].Value.ToString());
+                    int ni = int.Parse(row.Cells[1].Value.ToString());
+
+                    xiList.Add(xi);
+                    niList.Add(ni);
+                }
+            }
+
+            // Poisson hesaplayıcıyı başlat
+            var calculator = new PoissonCalculator();
+
+            // M değerini hesapla
+            double M = calculator.CalculateM(xiList, niList);
+            label1.Text = $"M (Lambda): {M}";
+
+            // Beklenen frekansları hesapla
+            double totalObservations = niList.Sum();
+            var expectedFrequencies = calculator.CalculateExpectedFrequencies(M, xiList, totalObservations);
+
+            if (!dataGridView1.Columns.Contains("Beklenen Frekans"))
+            {
+                dataGridView1.Columns.Add("BeklenenFrekans", "Beklenen Frekans");
+            }
+
+            // Beklenen frekansları ilgili satırlara yaz
+            for (int i = 0; i < expectedFrequencies.Length; i++)
+            {
+                // DataGridView'deki ilgili satıra beklenen frekansı yaz
+                if (i < dataGridView1.Rows.Count - 1) // Boş satırı hariç tut
+                {
+                    dataGridView1.Rows[i].Cells["BeklenenFrekans"].Value = expectedFrequencies[i].ToString("F2"); // İki ondalıklı format
+                }
+            }
+    }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class PoissonCalculator
+    {
+        // M (Lambda) değerini hesaplar
+        public double CalculateM(List<int> xiList, List<int> niList)
+        {
+            double sumXiNi = 0;
+            double sumNi = 0;
+
+            for (int i = 0; i < xiList.Count; i++)
+            {
+                sumXiNi += xiList[i] * niList[i];
+                sumNi += niList[i];
+            }
+
+            return sumXiNi / sumNi; // M (Lambda) değeri
+        }
+
+        // Beklenen frekansları hesaplar
+        public double[] CalculateExpectedFrequencies(double lambda, List<int> xiList, double totalObservations)
+        {
+            var expectedFrequencies = new double[xiList.Count];
+            for (int i = 0; i < xiList.Count; i++)
+            {
+                int xi = xiList[i];
+                double probability = Math.Exp(-lambda) * Math.Pow(lambda, xi) / Factorial(xi);
+                expectedFrequencies[i] = probability * totalObservations;
+            }
+
+            return expectedFrequencies;
+        }
+
+        // Faktöriyel hesaplama
+        private int Factorial(int n)
+        {
+            if (n == 0 || n == 1)
+                return 1;
+
+            int result = 1;
+            for (int i = 2; i <= n; i++)
+                result *= i;
+
+            return result;
+        }
+    }
+
+    public class DataReader
+    {
+        public List<int> XiList { get; private set; }
+        public List<int> NiList { get; private set; }
+
+        public DataReader()
+        {
+            XiList = new List<int>();
+            NiList = new List<int>();
+        }
+
+        // Dosyadan veri oku
+        public void LoadData(string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException("Dosya bulunamadı!");
+
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines)
+            {
+                var parts = line.Split(','); // xᵢ ve nᵢ arasına virgül koyulduğunu varsayıyoruz
+                XiList.Add(int.Parse(parts[0]));
+                NiList.Add(int.Parse(parts[1]));
+            }
+        }
+    }
+
+    public class ChiSquareTest
+    {
+        // Chi-Kare değerini hesaplar
+        public double CalculateChiSquare(List<int> observed, List<double> expected)
+        {
+            double chiSquare = 0.0;
+
+            for (int i = 0; i < observed.Count; i++)
+            {
+                if (expected[i] > 0) // Beklenen frekans sıfır olmamalı
+                {
+                    chiSquare += Math.Pow(observed[i] - expected[i], 2) / expected[i];
+                }
+            }
+
+            return chiSquare;
+        }
+
+        // p-Değeri hesaplar
+        public double CalculatePValue(double chiSquare, int degreesOfFreedom)
+        {
+            // MathNet.Numerics ile p-değeri hesaplama
+            return 1.0 - ChiSquared.CDF(degreesOfFreedom, chiSquare);
+        }
+
+        public double CalculateMean(List<int> xiList, List<int> niList)
+        {
+            double sumXiNi = 0;
+            double sumNi = 0;
+
+            for (int i = 0; i < xiList.Count; i++)
+            {
+                sumXiNi += xiList[i] * niList[i];
+                sumNi += niList[i];
+            }
+
+            return sumXiNi / sumNi; // Standart Ortalama
+
+        }
+
+        public double CalculateVariance(List<int> xiList, List<int> niList, double mean)
+        {
+            double sumXiMinusMeanSquared = 0;
+            double sumNi = 0;
+
+            for (int i = 0; i < xiList.Count; i++)
+            {
+                sumXiMinusMeanSquared += niList[i] * Math.Pow(xiList[i] - mean, 2);
+                sumNi += niList[i];
+            }
+
+            return sumXiMinusMeanSquared / sumNi; // Standart Varyans
+        }
+
+        // Teorik Momentler
+        public double GetTheoreticalMean(double lambda)
+        {
+            return lambda; // Poisson ortalaması
+        }
+
+        public double GetTheoreticalVariance(double lambda)
+        {
+            return lambda; // Poisson varyansı
+        }
+
+    }
+}
